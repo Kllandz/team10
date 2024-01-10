297745846d1f203352ef92d3d082c540bfaf6036
1104182014 <fx30015283@gmail.com>
2024-01-10 10:26:28 +0800

    期末

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -11,7 +11,7 @@ class Team extends Model
     protected $fillable = [
         'team',
         'year',
-        'division',
+        'divison',
         'rank',
         'rate',
         'pastrate',
